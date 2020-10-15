4629b84b6e6485a85d598755950d8d4a40956119
krove <krove@DESKTOP-Q5SED1R>
2020-10-16 03:07:19 +0530

    [Kashif]Modified:Find Cheapest Hotel based on Weekday and Weekend

--- src/main/java/com/cg/HotelReservation.java
+++ src/main/java/com/cg/HotelReservation.java
@@ -30,9 +30,43 @@ public class HotelReservation {
 		long minRent = Collections.min(hotelRentList);
 		Hotel cheapestHotel = hotel.stream().filter(hotel -> hotel.getRegularCustomerWeekdayRate() * days == minRent).findFirst()
 				.orElse(null);
-		System.out.println("Cheapest Hotel : "+ cheapestHotel.getHotelName()+" Cost : "+ minRent);
+		System.out.println("Hotel : "+ cheapestHotel.getHotelName()+" Cost : "+ minRent);
 		return cheapestHotel.getHotelName();
 	 }
+
+	public long hotelCost(Hotel hotel, long weekDay, long weekEnds) {
+		return hotel.getRegularCustomerWeekdayRate() * weekDay + hotel.getRegularCustomerWeekendRate() * weekEnds;
+	}
+	
+	public List<String> findCheapestHotelForRegularCustomersConsideringWeekdayAndWeekend(String start, String finish) throws ParseException {
+		convert(start, finish);
+		long days = getTotalDays(inDate, outDate);
+		long weekendDays = getWeekendDays(inDate, outDate);
+		long weekDays = days - weekendDays;
+		List<Long> hotelRentList = hotel.stream()
+				.map(hotel -> hotelCost(hotel, weekDays, weekendDays)).collect(Collectors.toList());
+		long minRent = Collections.min(hotelRentList);
+		List<String> cheapHotelList = hotel.stream()
+				.filter(hotel -> hotelCost(hotel, weekDays, weekendDays) == minRent)
+				.map(hotel -> hotel.getHotelName()).collect(Collectors.toList());
+		for (String hotel : cheapHotelList)
+			System.out.println("Hotel : " + hotel + " Cost : " + minRent);
+        return cheapHotelList;
+	}
+	
+	private long getWeekendDays(Date checkinDate, Date checkoutDate) {
+		long weekendDays = 0;
+		Calendar startCalendar = Calendar.getInstance();
+		startCalendar.setTime(checkinDate);
+		Calendar endCalendar = Calendar.getInstance();
+		endCalendar.setTime(checkoutDate);
+		for (; startCalendar.compareTo(endCalendar) <= 0; startCalendar.add(Calendar.DATE, 1)) {
+			int dayOfWeek = startCalendar.get(Calendar.DAY_OF_WEEK);
+			if (dayOfWeek == 0 || dayOfWeek == 6)
+				weekendDays++;
+		}
+		return weekendDays;
+	}
 	
 	private long getTotalDays(Date startDate, Date endDate) {
 		return ((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)) + 1;
